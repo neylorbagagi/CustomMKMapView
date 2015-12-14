55c6df356a4f7f035ff72f2093f22d7ab1fa1fae
Neylor <nwsbagagi@gmail.com>
2015-12-14 09:18:36 -0200

    Initial Commit

---
+++ CustomMKMapView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CustomMKMapView
+//
+//  Created by Neylor Bagagi on 12/14/15.
+//  Copyright © 2015 Neylor Bagagi. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ CustomMKMapView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CustomMKMapView
+//
+//  Created by Neylor Bagagi on 12/14/15.
+//  Copyright © 2015 Neylor Bagagi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
